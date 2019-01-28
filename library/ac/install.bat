ecfbc9cb097ec9d87f4b6ae81cbe7d1d012d45fc
Rizky Khapidsyah <45317201+RizkyKhapidsyah@users.noreply.github.com>
2019-01-29 01:30:17 +0700

    Add files via upload

---
+++ library/ac/install.bat
@@ -0,0 +1,44 @@
+cd C:\Windows\System32
+mkdir C:\Windows\System32\RS
+cd C:\Windows\System32\RS
+mkdir C:\Windows\System32\RS\ProgramPulsa
+cd C:\Windows\System32\RS\ProgramPulsa
+
+copy AeroSuite.ocx, C:\Windows\System32\RS\ProgramPulsa\AeroSuite.ocx
+copy ARButton.ocx, C:\Windows\System32\RS\ProgramPulsa\ARButton.ocx
+copy comdlg32.ocx, C:\Windows\System32\RS\ProgramPulsa\comdlg32.ocx
+copy cpvButton.ocx, C:\Windows\System32\RS\ProgramPulsa\cpvButton.ocx
+copy dcButton.ocx, C:\Windows\System32\RS\ProgramPulsa\dcButton.ocx
+copy FUSIONButtons.ocx, C:\Windows\System32\RS\ProgramPulsa\FUSIONButtons.ocx
+copy glxpbuttonz.ocx, C:\Windows\System32\RS\ProgramPulsa\glxpbuttonz.ocx
+copy HookMenu.ocx, C:\Windows\System32\RS\ProgramPulsa\HookMenu.ocx
+copy isButton.ocx, C:\Windows\System32\RS\ProgramPulsa\isButton.ocx
+copy MSADODC.ocx, C:\Windows\System32\RS\ProgramPulsa\MSADODC.ocx
+copy MSCAL.ocx, C:\Windows\System32\RS\ProgramPulsa\MSCAL.ocx
+copy MSDATGRD.ocx, C:\Windows\System32\RS\ProgramPulsa\MSDATGRD.ocx
+copy Richtx32.ocx, C:\Windows\System32\RS\ProgramPulsa\Richtx32.ocx
+copy XP_Engine.ocx, C:\Windows\System32\RS\ProgramPulsa\XP_Engine.ocx
+copy xTab.ocx, C:\Windows\System32\RS\ProgramPulsa\xTab.ocx
+copy MSCOMCTL.ocx, C:\Windows\System32\RS\ProgramPulsa\MSCOMCTL.ocx
+
+
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\AeroSuite.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\ARButton.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\comdlg32.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\cpvButton.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\dcButton.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\FUSIONButtons.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\glxpbuttonz.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\HookMenu.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\isButton.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\MSADODC.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\MSCAL.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\MSDATGRD.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\Richtx32.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\XP_Engine.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\xTab.ocx
+regsvr32 C:\Windows\System32\RS\ProgramPulsa\MSCOMCTL.ocx
+
+
+
+pause
